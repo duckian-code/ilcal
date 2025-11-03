7d3047e4d27113154504c28b7355790d5b223f9d
Lily <66748489+duckian-code@users.noreply.github.com>
2025-11-03 15:26:56 -0700

    Initial commit

---
+++ target/debug/deps/clical-d55873e8d1282858.d
@@ -0,0 +1,5 @@
+/home/anabelle/Scripts/clical/target/debug/deps/clical-d55873e8d1282858.d: src/main.rs
+
+/home/anabelle/Scripts/clical/target/debug/deps/libclical-d55873e8d1282858.rmeta: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/clical-fdd3e0a85f2e1ad6.d
@@ -0,0 +1,5 @@
+/home/anabelle/Scripts/clical/target/debug/deps/clical-fdd3e0a85f2e1ad6.d: src/main.rs
+
+/home/anabelle/Scripts/clical/target/debug/deps/libclical-fdd3e0a85f2e1ad6.rmeta: src/main.rs
+
+src/main.rs:
